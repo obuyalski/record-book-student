5a581dc317f5ade4a7e6ed7a8dfb3c3e4ff63b00
zidabah <zidabah@mail.ru>
2016-09-19 19:39:50 +0300

    Changes in Test2 Project

--- Test2/src/com/company/Main.java
+++ Test2/src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Govno");
+        System.out.println("Sanya molodec!");
         // write your code here
     }
 }
